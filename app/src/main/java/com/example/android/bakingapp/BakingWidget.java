c0703ba99762213cd46c7a4bcca1fe47d10f3cf4
Allen Soberano <allensoberano@gmail.com>
2018-07-13 06:13:12 -0700

    Widget Working

---
+++ app/src/main/java/com/example/android/bakingapp/BakingWidget.java
@@ -0,0 +1,64 @@
+package com.example.android.bakingapp;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class BakingWidget extends AppWidgetProvider {
+
+    private String APP_TITLE = "Baking App";
+    private String WIDGET_INGREDIENTS = "ingredients";
+    private String WIDGET_RECIPE_NAME = "recipeName";
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId, String ingredients, String recipeName) {
+
+        CharSequence widgetText = ingredients;
+        CharSequence name = recipeName;
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baking_widget);
+        views.setTextViewText(R.id.recipe_name, name);
+        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    private String getIngredients(Context context){
+        SharedPreferences pref = context.getSharedPreferences(APP_TITLE, 0);
+        String ingredientList = pref.getString(WIDGET_INGREDIENTS, null);
+        return ingredientList;
+    }
+
+    private String getRecipeName(Context context){
+        SharedPreferences pref = context.getSharedPreferences(APP_TITLE, 0);
+        String recipeName = pref.getString(WIDGET_RECIPE_NAME, null);
+        return recipeName;
+    }
+
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId, getIngredients(context), getRecipeName(context));
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
--- app/src/main/java/com/example/android/bakingapp/ui/RecipeDetailsFragment.java
+++ app/src/main/java/com/example/android/bakingapp/ui/RecipeDetailsFragment.java
@@ -1,6 +1,7 @@
 package com.example.android.bakingapp.ui;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
@@ -24,20 +25,20 @@ import java.util.List;
 
 public class RecipeDetailsFragment extends Fragment implements RecipeDetailRVAdapter.ItemClickListener {
 
-   private Recipe mRecipeSent;
-   private RecyclerView mRecipeList;
-   OnStepClickListener mCallback;
+    private Recipe mRecipeSent;
+    private RecyclerView mRecipeList;
+    OnStepClickListener mCallback;
 
-   //Instant States
-   private Parcelable rvState;
-   private final static String LIST_STATE_KEY = "rv_state";
+    //Instant States
+    private Parcelable rvState;
+    private final static String LIST_STATE_KEY = "rv_state";
 
-   //Mandatory constructor for instantiating the fragment
-   public RecipeDetailsFragment(){
+    //Mandatory constructor for instantiating the fragment
+    public RecipeDetailsFragment() {
 
-   }
+    }
 
-    public interface OnStepClickListener{
+    public interface OnStepClickListener {
 
         void onStepSelected(ArrayList<Step> steps, int position);
     }
@@ -58,31 +59,30 @@ public class RecipeDetailsFragment extends Fragment implements RecipeDetailRVAda
     }
 
     @Nullable
-   @Override
-   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
 
-      mRecipeSent = getArguments().getParcelable("recipe");
+        mRecipeSent = getArguments().getParcelable("recipe");
 
-      //Inflate the RecipeMainFragment layout
-      View rootView = inflater.inflate(R.layout.fragment_recipe_details, container, false);
+        //Inflate the RecipeMainFragment layout
+        View rootView = inflater.inflate(R.layout.fragment_recipe_details, container, false);
 
         //setTitleActionBar();
 
-      mRecipeList = rootView.findViewById(R.id.rv_recipe_details);
-      mRecipeList.setLayoutManager(new LinearLayoutManager(getContext()));
-      RecipeDetailRVAdapter recipeDetailRVAdapter= new RecipeDetailRVAdapter(mRecipeSent, this);
-      mRecipeList.setAdapter(recipeDetailRVAdapter);
-      recipeDetailRVAdapter.notifyDataSetChanged();
-      //showRecipe(mRecipeSent);
-
+        mRecipeList = rootView.findViewById(R.id.rv_recipe_details);
+        mRecipeList.setLayoutManager(new LinearLayoutManager(getContext()));
+        RecipeDetailRVAdapter recipeDetailRVAdapter = new RecipeDetailRVAdapter(mRecipeSent, this);
+        mRecipeList.setAdapter(recipeDetailRVAdapter);
+        recipeDetailRVAdapter.notifyDataSetChanged();
+        //showRecipe(mRecipeSent);
 
 
         TextView ingredients = rootView.findViewById(R.id.tv_ingredient_list);
         ingredients.setText(buildString(mRecipeSent.getIngredients()));
 
-      return rootView;
-   }
+        return rootView;
+    }
 
     private void showRecipe(Recipe recipe) {
         RecipeDetailRVAdapter recipeRVAdapter = new RecipeDetailRVAdapter(recipe, this);
@@ -91,24 +91,34 @@ public class RecipeDetailsFragment extends Fragment implements RecipeDetailRVAda
 
     }
 
-   @Override
-   public void onItemClick(int position) {
-       mCallback.onStepSelected(mRecipeSent.getSteps(), position);
+    @Override
+    public void onItemClick(int position) {
+        mCallback.onStepSelected(mRecipeSent.getSteps(), position);
 
-   }
+    }
 
     //Builds a string of ingredients with line breaks
-   private String buildString(List<Ingredient> ingredients){
+    private String buildString(List<Ingredient> ingredients) {
 
-       StringBuilder builder = new StringBuilder();
-       for (Ingredient ingredient : ingredients) {
-           builder.append(ingredient.getQuantity() + " "+ ingredient.getMeasure() + " " + ingredient.getIngredient() + "\n");
-       }
+        StringBuilder builder = new StringBuilder();
+        for (Ingredient ingredient : ingredients) {
+            builder.append(ingredient.getQuantity() + " " + ingredient.getMeasure() + " " + ingredient.getIngredient() + "\n");
+        }
 
-       return builder.toString();
-       }
+        saveIngredientsToSharedPref(builder.toString());
+        return builder.toString();
+    }
 
+    private void saveIngredientsToSharedPref(String ingredients){
+        //Reference: https://www.journaldev.com/9412/android-shared-preferences-example-tutorial
+        //save to shared preferences for widget
+        SharedPreferences pref = getContext().getSharedPreferences("BakingApp", 0);
+        SharedPreferences.Editor editor = pref.edit();
 
+        editor.putString("ingredients", ingredients);
+        editor.putString("recipeName", mRecipeSent.getName());
+        editor.commit();
+    }
 
 
     @Override
@@ -123,7 +133,7 @@ public class RecipeDetailsFragment extends Fragment implements RecipeDetailRVAda
         super.onActivityCreated(savedInstanceState);
         if (savedInstanceState != null) {
             // Restore last state for checked position.
-           // mRecipeList = savedInstanceState.getParcelable(LIST_STATE_KEY);
+            // mRecipeList = savedInstanceState.getParcelable(LIST_STATE_KEY);
             rvState = savedInstanceState.getParcelable(LIST_STATE_KEY);
             mRecipeList.getLayoutManager().onRestoreInstanceState(rvState);
         }
